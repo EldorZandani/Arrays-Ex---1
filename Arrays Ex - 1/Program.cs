f97a3219005482424be504a957c0bf7b14c2e342
EldorZandani <fox18966@gmail.com>
2022-03-24 15:44:46 +0200

    how to do an Array

---
+++ Arrays Ex - 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Ex___1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+            int i;
+            Console.WriteLine("please enter " + grades.Length + " grades:");
+            for (i = 0; i < grades.Length; i++)
+                grades[i] = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < grades.Length; i++)
+                Console.Write(grades[i] + " ");
+        }
+    }
+    
+}
